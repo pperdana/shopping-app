c1ded60ce0f98595620da1760c533da096965595
Panji <panji9502@gmail.com>
2022-12-29 04:12:08 +0700

    add notifications with material ui

--- src/App.js
+++ src/App.js
@@ -1,17 +1,61 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Auth from "./components/Auth";
 import Layout from "./components/Layout";
+import Notification from "./Notification";
+import { uiActions } from "./store/ui-slice";
+let isFirstRender = true;
 
 function App() {
   const isLoggedIn = useSelector((state) => state.auth.isLoggedIn);
-  console.log("isLoggedIn:", isLoggedIn);
-
-  const cartItems = useSelector((state) => state.cart.itemsList);
-  console.log(cartItems);
+  const cart = useSelector((state) => state.cart);
+  const notification = useSelector((state) => state.ui.notification);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    if (isFirstRender) {
+      isFirstRender = false;
+      return;
+    }
+    const sendRequest = async () => {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Send Request",
+          type: "warning",
+        })
+      );
+      const res = await fetch(
+        "https://redux-http-cb951-default-rtdb.firebaseio.com/cartItems.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        }
+      );
+      const data = await res.json();
+      dispatch(
+        uiActions.showNotification({
+          message: "Sent Request To Database Successfully",
+          open: true,
+          type: "success",
+        })
+      );
+    };
+    sendRequest().catch((err) => {
+      dispatch(
+        uiActions.showNotification({
+          open: true,
+          message: "Sending message Failed",
+          type: "error",
+        })
+      );
+    });
+  }, [cart]);
   return (
     <div className="App">
+      {notification && (
+        <Notification type={notification.type} message={notification.message} />
+      )}
       {!isLoggedIn && <Auth />}
       {isLoggedIn && <Layout />}
     </div>
---
+++ src/Notification.js
@@ -0,0 +1,28 @@
+import { Alert } from "@mui/material";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
+import { uiActions } from "./store/ui-slice";
+
+const Notification = ({ type, message }) => {
+  const dispatch = useDispatch();
+  const notification = useSelector((state) => state.ui.notification);
+  const handleClose = () => {
+    dispatch(
+      uiActions.showNotification({
+        open: false,
+      })
+    );
+  };
+  return (
+    <div>
+      {notification.open && (
+        <Alert onClose={handleClose} severity={type}>
+          {message}
+        </Alert>
+      )}
+    </div>
+  );
+};
+
+export default Notification;
