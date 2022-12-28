61e64fbd779e1383b142b5bb98f1c98784f7b363
Panji <panji9502@gmail.com>
2022-12-29 01:48:17 +0700

    add action functionality into cart components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import Cart from "./Cart";
 import "./Header.css";
+import { authActions } from "../store/auth-slice";
+
 const Header = () => {
+  const dispatch = useDispatch();
+  const logoutHandler = () => {
+    dispatch(authActions.logout());
+  };
   return (
     <header>
       <nav className="header-nav">
@@ -16,6 +23,11 @@ const Header = () => {
           </li>
           <li>
             <Cart />
+          </li>
+          <li>
+            <button onClick={logoutHandler} className="logout-btn">
+              Log Out
+            </button>
           </li>
         </ul>
       </nav>
